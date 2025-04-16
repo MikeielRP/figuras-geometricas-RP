5c9c076302b67f98f4437e2c68073bfdc04d580b
MikeielRP <tu-email@ejemplo.com>
2025-04-16 11:21:43 -0500

    Cambios realizados

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Figuras Geometricas
+## Proposito del Repositorio
+* Este repositorio es un ejemplo en Python que calcula áreas de figuras geométricas usando herencia y clases abstractas.
+## Cambios Realizados
+1. Se añadieron Docstrings en las lines 1, 20, 30 7 40
+2. Se elimino el pass en la linea 16
+3. Se añadio el super().__init__()  en las clases que heredaban de la clase figurageometrica
+4. Se desactivo too-few-public-methods para las clases con 1 solo emtodo ya que no eran necesarias mas
 ## Desarrollador
-| Apellidos y nombres |
-| --- |
-|      |
+### Rojas Peña William Mikeiel
---
+++ figura_geometrica.py
@@ -0,0 +1,65 @@
+"""Programa en python que calcula el área de las figuras geométricas"""
+
+from abc import ABC, abstractmethod
+
+
+# pylint: disable=too-few-public-methods
+class FiguraGeometrica(ABC):
+    """Clase abstracta que define la interfaz para calcular áreas."""
+    def __init__(self, base=None, altura=None):
+        self.base = base
+        self.altura = altura
+
+    @abstractmethod
+    def calcular_area(self):
+        """Método abstracto para calcular el área de una figura."""
+
+
+# pylint: disable=too-few-public-methods
+class Rectangulo(FiguraGeometrica):
+    """Clase para calcular el área de un rectángulo."""
+    def __init__(self, base, altura):
+        super().__init__(base, altura)
+
+    def calcular_area(self):
+        return self.base * self.altura
+
+
+# pylint: disable=too-few-public-methods
+class Triangulo(FiguraGeometrica):
+    """Clase para calcular el área de un triángulo."""
+    def __init__(self, base, altura):
+        super().__init__(base, altura)
+
+    def calcular_area(self):
+        return (self.base * self.altura) / 2
+
+
+# pylint: disable=too-few-public-methods
+class Circulo(FiguraGeometrica):
+    """Clase para calcular el área de un círculo."""
+    def __init__(self, radio):
+        super().__init__()  # Aunque no se usen base/altura, se llama igual
+        self.radio = radio
+        self.pi = 3.14159
+
+    def calcular_area(self):
+        return self.pi * (self.radio ** 2)
+
+
+# Variables globales
+BASE_RECTANGULO = 10
+ALTURA_RECTANGULO = 5
+BASE_TRIANGULO = 7
+ALTURA_TRIANGULO = 4
+RADIO_CIRCULO = 3
+
+# Ejecución principal
+if __name__ == "__main__":
+    rectangulo = Rectangulo(BASE_RECTANGULO, ALTURA_RECTANGULO)
+    triangulo = Triangulo(BASE_TRIANGULO, ALTURA_TRIANGULO)
+    circulo = Circulo(RADIO_CIRCULO)
+
+    print(f"El área del rectángulo es: {rectangulo.calcular_area()}")
+    print(f"El área del triángulo es: {triangulo.calcular_area()}")
+    print(f"El área del círculo es: {circulo.calcular_area()}")
